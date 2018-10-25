56b5ce0f152f38246e09c32034790195b5138021
Mingliang233 <Mingliang233@outlook.com>
2018-10-25 09:34:50 +0800

    更改为wallet.jsp

--- src/main/java/org/ht/controller/WalletController.java
+++ src/main/java/org/ht/controller/WalletController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WalletController {
 	@RequestMapping("/wallet")
 	public String getWallet(Model model){
-		return "account";
+		return "wallet";
 	}
 }
